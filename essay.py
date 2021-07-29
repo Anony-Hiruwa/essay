e99a8da26252ae57f9a86198edacf8383bfa89f6
HIRUWA <87813313+Dark-Knight-Hiruwa@users.noreply.github.com>
2021-07-28 21:43:43 -0700

    update essay.py

--- essay.py
+++ essay.py
@@ -1,5 +1,7 @@
                     #CODE BY HIRUWA
 from colorama import Fore, init
+from os import system as sys
+import time
 
 #color
 init()
@@ -36,16 +38,32 @@ det = '''\033[91m===============================================================
 ==================================================================='''
 print(ban)
 print(det)
-wel=( '\033[92m' "")
+
+#essay quizz
+wel=( '\033[92m' "WELCOME TO ESSAY TOOL BY HIRUWA")
 print (wel.center(70))
-fname=input( '\033[33m'  "What is Your FIRST NAME : ")
-lname=input( '\033[33m' "What is Your LAST NAME  : ")
-print("")
-agey=int(input( '\033[33m'  "What is Your Birth Year  : "))
+
+fname=input('\033[93m'"What is Your FIRST NAME : ")
+lname=input('\033[93m'"What is Your LAST NAME  : ")
+agey=int(input( '\033[93m'"What is Your Birth Year  : "))
 age = 2021 - (agey)
-ad=input('\033[33m' "What is your Addres : ")
-ju=int(input( '\033[33m'  "You junior or senior student in your School : "))
+if age>=100 :
+    print('\033[091m',fname,"YOU ENTERD WRONG ANSWER !!!!!")
+else:
+    print(fname,"Your Age is : ",age)
+ad=input('\033[93m' "What is your Addres : ")
+ju=input('\033[93m'"You junior or senior student in your School : ")
+mj=input("Your mother Job : ")
+fj=input("Your father Job : ")
+fs=input("What is your favourit subject : ")
+pn=input("What is your favourit name in your name ' PET NAME ' : ")
+
+#system cmd
+sys("clear")
+
+#output
+print("YOUR ESSAY IS WE WRITE NOW ... Please wait few sec.........".center(50))
+time.sleep(4)
+print(' \033[92m' "MY SELF".center(50))
 print("")
-print(' \033[33m' "MY SELF".center(70))
-print('"')
-print('\033[92m' "My name is",fname,lname,".","I am a",ju,"in my school.I am also the youngest child in my family.","I have two older brothers and two older sisters.")
+print('\033[92m' "My name is",fname,lname,".","I am a",ju,"in my school.I am also the youngest child in my family.","I live in",ad,"My mum is a",mj,"and my dad is a",fj,"\nIt is a common knowledge that I am a good student and I love to study a lot.","My favorite subjects is",fs,"I’m what a lot of people call a",pn,"I have every intention of applying to a reputable university and obtaining a degree in one of these fields.I am responsible and hardworking, so I study hard to obtain good grades.\n\n","I have always been treated like a baby, by my parents, siblings, teachers and basically everyone who is not in my age group.Sometimes even my friends talk to me like I am a child, which is why I basically love school and reading books.Books don’t talk back at you and they make me feel important in this gigantic universe.\n\nThis is not to say that I don’t love all the care and attention. I do, it’s just that sometimes it can get stifling. However, I am quite content with my life.")
